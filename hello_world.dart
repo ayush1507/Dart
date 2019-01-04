3afc62bfeb906994a4881f7f4d0a240c7813a4b2
ayush1507 <35962693+ayush1507@users.noreply.github.com>
2019-01-04 20:41:29 +0530

    Update hello_world.dart
    Updated to How to write comments?.

--- hello_world.dart
+++ hello_world.dart
@@ -1,7 +1,11 @@
 
+//This is my first program in Dart.
 void main(){
-	print("Hello World");
-  
+	
+  print("Hello World");
+	
+  // Use " // " to write your comments.
+	
   print("My first program in Dart");
   
   print(85/5);
