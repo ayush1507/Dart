f58178d7f9b6ea9e0d5f83e13fc405f9e2508c29
ayush1507 <35962693+ayush1507@users.noreply.github.com>
2019-01-04 22:29:09 +0530

    Create 2-variables_datatypes.dart

---
+++ 2-variables_datatypes.dart
@@ -0,0 +1,14 @@
+
+void main(){
+
+  //Strings
+  String fruit = "apple";
+  print(fruit);
+  
+  //Double
+  double qwerty = 8536.6;
+  print (qwerty);
+ 
+  //Or we can use "var" instead of Stirngs , Double, Boolean etc.
+  
+}
