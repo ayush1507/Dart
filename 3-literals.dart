f2075c03339720c98c0098359e60bc6afeebc519
ayush1507 <35962693+ayush1507@users.noreply.github.com>
2019-01-05 18:07:28 +0530

    Create 3-literals.dart

---
+++ 3-literals.dart
@@ -0,0 +1,13 @@
+
+
+void main(){
+
+  //Literals
+  String name = 'Ayush';
+  
+  //String Interpolation
+  print('My name is $name');
+	print('Number of characters is ${name.length}');
+  
+  //To combine two strings use '$' instead of '+' sign.
+}
