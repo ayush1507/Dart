282f720ed0df26f9c980945dae68523524c7af74
ayush1507 <35962693+ayush1507@users.noreply.github.com>
2019-01-05 19:23:34 +0530

    Create 6-swtich_statements.dart

---
+++ 6-swtich_statements.dart
@@ -0,0 +1,21 @@
+
+void main(){
+
+  //Switch statements
+  int salary = 50000;
+  switch(salary){
+      
+    case 50000:
+      print('Your salary is good enough');
+      break;
+      
+    case 20000:
+      print('Your salary is not that good');
+      break;
+      
+    default:
+      print('Ok! Work Hard');
+      
+  }
+  
+  }
