fb7ba0135096f39650fbaba365b001a44e073fec
ayush1507 <35962693+ayush1507@users.noreply.github.com>
2019-01-05 18:37:50 +0530

    Create 4-if_else.dart

---
+++ 4-if_else.dart
@@ -0,0 +1,17 @@
+
+void main(){
+
+  var age = 18;
+  
+  //Use of IF ELSE.
+  
+  if(age <12){
+    print('Age is less than 12');
+  }else if (age==15){
+    print('Age is 15');
+   
+  }else{
+    print('Age is $age');
+  }
+  
+  }
