51a5ff5312bed9ae9da21b9a18a7501528273701
ayush1507 <35962693+ayush1507@users.noreply.github.com>
2019-01-06 17:10:37 +0530

    Create 9-SHORT-HAND-SYNTAX.dart

---
+++ 9-SHORT-HAND-SYNTAX.dart
@@ -0,0 +1,10 @@
+
+void main(){
+  
+  perimeter(25,52);
+
+ }
+
+//In defining a Function we can use SHORT HAND SYNTAX (=>) instead of defining it in curly brackets.
+void perimeter(int a , int b)=>
+  print('Perimeter is  {2*(a+b)}');
